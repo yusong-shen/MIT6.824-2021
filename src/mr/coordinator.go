7ec8434a6b26044d9154df8f965f54c1bb49b0e3
Yusong Shen <yusongtju@gmail.com>
2022-01-03 21:28:50 -0600

    Let the coordinator assign reduce task to worker once all the map tasks are completed.
    Note: Current version of tests has issue, since different initializeMapTask will interfere with each other.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -44,7 +44,9 @@ type SafeStatusMap struct {
 // key: string representation of task
 // value: task status, 1-idle, 2-in progress, 3-completed
 var taskStatusMap SafeStatusMap = SafeStatusMap{m: make(map[string]int)}
-var idleTasksQueue chan Task = make(chan Task, 100)
+var idleMapTasksQueue chan Task = make(chan Task, 100)
+var idleReduceTaskQueue chan Task = make(chan Task, 100)
+var taskInitializationMutex sync.Mutex
 
 // atomic interger
 var remainingMapTasksCnt int32
@@ -56,14 +58,14 @@ func (c *Coordinator) initializeMapTasks() {
 	atomic.StoreInt32(&remainingReduceTasksCnt, -1)
 	for i, file := range c.inputfiles {
 		t := Task{TaskType: 1, Inputfiles: []string{file}, TaskId: i}
-		fmt.Printf("Initializing task: %v\n", t)
+		fmt.Printf("Initializing map task: %v\n", t)
 		taskStatusMap.mu.Lock()
 		taskStatusMap.m[t.toString()] = 1
 		taskStatusMap.mu.Unlock()
 		// if queue is full, it will block until there is some consumer complete some tasks
-		idleTasksQueue <- t
+		idleMapTasksQueue <- t
 	}
-	fmt.Printf("Queue Len: %d\n", len(idleTasksQueue))
+	fmt.Printf("IdleMapTaskQueue Len: %d\n", len(idleMapTasksQueue))
 
 }
 
@@ -95,15 +97,39 @@ func (c *Coordinator) checkWorkerStatus(workerId string) bool {
 
 func (c *Coordinator) AskTask(args *AskTaskArgs, reply *AskTaskReply) error {
 	fmt.Printf("WorkerId : %v\n", args.WorkerId)
-	t := <-idleTasksQueue
-	fmt.Printf("Len: %d\n", len(idleTasksQueue))
-	fmt.Printf("Task: %v\n", t)
-	reply.T = t
-	reply.ReduceTasksCnt = c.reduceTasksCnt
-	// mark the task as in progress
-	taskStatusMap.mu.Lock()
-	taskStatusMap.m[t.toString()] = 2
-	taskStatusMap.mu.Unlock()
+
+	if len(idleMapTasksQueue) == 0 && c.getRemainingMapTasksCnt() != 0 {
+		// return empty reply without any task since there is not idle map task
+		// but reduce tasks haven't been available yet.
+		// worker need to wait for all the map tasks to be completed
+		return nil
+	}
+	// assign idle map task when available
+	if len(idleMapTasksQueue) != 0 {
+		t := <-idleMapTasksQueue
+		fmt.Printf("Assign Map Task: %v\n", t)
+		fmt.Printf("idleMapTasksQueue len: %d\n", len(idleMapTasksQueue))
+		reply.T = t
+		reply.ReduceTasksCnt = c.reduceTasksCnt
+		// mark the task as in progress
+		taskStatusMap.mu.Lock()
+		taskStatusMap.m[t.toString()] = 2
+		taskStatusMap.mu.Unlock()
+		return nil
+	}
+	// assign reduce task when available
+	if len(idleReduceTaskQueue) != 0 {
+		t := <-idleReduceTaskQueue
+		fmt.Printf("Assign Reduce Task: %v\n", t)
+		fmt.Printf("idleReduceTaskQueue len: %d\n", len(idleReduceTaskQueue))
+		reply.T = t
+		reply.ReduceTasksCnt = c.reduceTasksCnt
+		// mark the task as in progress
+		taskStatusMap.mu.Lock()
+		taskStatusMap.m[t.toString()] = 2
+		taskStatusMap.mu.Unlock()
+	}
+
 	return nil
 }
 
@@ -118,8 +144,13 @@ func (c *Coordinator) ReportTaskStatus(args *ReportTaskStatusArgs, reply *Report
 	taskStatusMap.mu.Lock()
 	taskStatusMap.m[task.toString()] = 3
 	taskStatusMap.mu.Unlock()
-	if c.getRemainingMapTasksCnt() == 0 {
-		c.initializeReduceTasks()
+	if c.getRemainingMapTasksCnt() == 0 && c.getRemainingReduceTasksCnt() == -1 {
+		taskInitializationMutex.Lock()
+		// check the count again to ensure reduce tasks haven't been initialized yet
+		if c.getRemainingReduceTasksCnt() == -1 {
+			c.initializeReduceTasks()
+		}
+		taskInitializationMutex.Unlock()
 	}
 	return nil
 }
@@ -133,7 +164,29 @@ func (c *Coordinator) getRemainingReduceTasksCnt() int32 {
 }
 
 func (c *Coordinator) initializeReduceTasks() {
-	// TODO
+	atomic.StoreInt32(&remainingReduceTasksCnt, int32(c.reduceTasksCnt))
+	for i := 0; i < c.reduceTasksCnt; i++ {
+		task := Task{TaskType: 2, Inputfiles: c.getReducerInputFiles(i), TaskId: i}
+		// update the task status map
+		fmt.Printf("Initializing reduce task: %v\n", task)
+		taskStatusMap.mu.Lock()
+		taskStatusMap.m[task.toString()] = 1
+		taskStatusMap.mu.Unlock()
+		idleReduceTaskQueue <- task
+	}
+	fmt.Printf("IdleReduceTaskQueue Len: %d\n", len(idleReduceTaskQueue))
+
+}
+
+func (c *Coordinator) getReducerInputFiles(reduceTaskId int) []string {
+	inputFiles := make([]string, 0)
+	// file name looks like mr-X-Y, where X is the Map task number,
+	// and Y is the reduce task number
+	for i := 0; i < len(c.inputfiles); i++ {
+		filename := fmt.Sprintf("mr-%v-%v", i, reduceTaskId)
+		inputFiles = append(inputFiles, filename)
+	}
+	return inputFiles
 }
 
 //
@@ -158,9 +211,6 @@ func (c *Coordinator) server() {
 //
 func (c *Coordinator) Done() bool {
 	ret := false
-
-	// TODO: Your code here.
-
 	if atomic.LoadInt32(&remainingMapTasksCnt) == 0 && atomic.LoadInt32(&remainingReduceTasksCnt) == 0 {
 		ret = true
 	}
--- src/mr/coordinator_test.go
+++ src/mr/coordinator_test.go
@@ -51,6 +51,26 @@ func TestAskTaskRpc(t *testing.T) {
 	assert.Equal(t, 2, status)
 }
 
+func TestAskTaskRpc_NotIdleMapTask(t *testing.T) {
+	c := Coordinator{inputfiles: []string{"file1"}, reduceTasksCnt: 10}
+	c.initializeMapTasks()
+	args := AskTaskArgs{WorkerId: "2"}
+	var reply AskTaskReply
+
+	// take the only idle map task
+	err := c.AskTask(&args, &reply)
+	fmt.Println(reply.T.toString())
+	assert.NoError(t, err)
+
+	// ask the task again, should get empty reply
+	args = AskTaskArgs{WorkerId: "2"}
+	reply = AskTaskReply{}
+	err = c.AskTask(&args, &reply)
+	fmt.Println(reply.T.toString())
+	assert.NoError(t, err)
+	assert.Equal(t, AskTaskReply{}, reply)
+}
+
 func TestInitializeMapTasks(t *testing.T) {
 	c := Coordinator{inputfiles: []string{"file1", "file2"}}
 	c.initializeMapTasks()
@@ -97,5 +117,101 @@ func TestReportTaskStatusRpc(t *testing.T) {
 	err = c.ReportTaskStatus(&arg, &reply)
 	assert.NoError(t, err)
 	assert.Equal(t, int32(-2), c.getRemainingReduceTasksCnt())
+}
 
+func TestGetReducerInputFiles(t *testing.T) {
+	c := Coordinator{inputfiles: []string{"file1", "file2"}, reduceTasksCnt: 10}
+	reduceInputFiles := c.getReducerInputFiles(2)
+	assert.Equal(t, []string{"mr-0-2", "mr-1-2"}, reduceInputFiles)
+}
+
+func TestInitializeReduceTasks(t *testing.T) {
+	c := Coordinator{inputfiles: []string{"file1", "file2"}, reduceTasksCnt: 3}
+	c.initializeReduceTasks()
+	status, ok := c.checkTaskStatus(Task{TaskType: 2, Inputfiles: []string{"mr-0-0", "mr-1-0"}, TaskId: 0})
+	assert.True(t, ok)
+	assert.Equal(t, 1, status)
+
+	status, ok = c.checkTaskStatus(Task{TaskType: 2, Inputfiles: []string{"mr-0-1", "mr-1-1"}, TaskId: 1})
+	assert.True(t, ok)
+	assert.Equal(t, 1, status)
+
+	status, ok = c.checkTaskStatus(Task{TaskType: 2, Inputfiles: []string{"mr-0-2", "mr-1-2"}, TaskId: 2})
+	assert.True(t, ok)
+	assert.Equal(t, 1, status)
+}
+
+func TestAskTaskRpc_AllMapTasksComplete_ShouldGetReduceTask(t *testing.T) {
+	c := Coordinator{inputfiles: []string{"file1"}, reduceTasksCnt: 2}
+	c.initializeMapTasks()
+	argsAskTask := AskTaskArgs{WorkerId: "2"}
+	replyAskTask := AskTaskReply{}
+
+	// set up:
+	// take the only idle map task
+	err := c.AskTask(&argsAskTask, &replyAskTask)
+	fmt.Println(replyAskTask.T.toString())
+	assert.NoError(t, err)
+
+	// complete the task
+	argReportTask := ReportTaskStatusArgs{T: replyAskTask.T, Status: "Completed"}
+	replyReportTask := ReportTaskStatusReply{}
+	err = c.ReportTaskStatus(&argReportTask, &replyReportTask)
+	assert.NoError(t, err)
+
+	// act:
+	// now all the map task completed, ask task again
+	argsAskTask = AskTaskArgs{WorkerId: "3"}
+	replyAskTask = AskTaskReply{}
+	err = c.AskTask(&argsAskTask, &replyAskTask)
+	fmt.Println(replyAskTask.T.toString())
+	assert.NoError(t, err)
+	// assert worker should get reduce task
+	assert.Equal(t, 2, replyAskTask.T.TaskType)
+	assert.Equal(t, []string{"mr-0-0"}, replyAskTask.T.Inputfiles)
+	assert.Equal(t, 2, replyAskTask.ReduceTasksCnt)
+
+	// assert task status is 2 (in progress)
+	status, ok := c.checkTaskStatus(replyAskTask.T)
+	assert.True(t, ok)
+	assert.Equal(t, 2, status)
+}
+
+func TestDone(t *testing.T) {
+	c := Coordinator{inputfiles: []string{"file1"}, reduceTasksCnt: 1}
+	c.initializeMapTasks()
+	argsAskTask := AskTaskArgs{WorkerId: "2"}
+	replyAskTask := AskTaskReply{}
+
+	// set up:
+	// take the only idle map task
+	err := c.AskTask(&argsAskTask, &replyAskTask)
+	fmt.Println(replyAskTask.T.toString())
+	assert.NoError(t, err)
+	assert.False(t, c.Done())
+
+	// complete the map task
+	argReportTask := ReportTaskStatusArgs{T: replyAskTask.T, Status: "Completed"}
+	replyReportTask := ReportTaskStatusReply{}
+	err = c.ReportTaskStatus(&argReportTask, &replyReportTask)
+	assert.NoError(t, err)
+	assert.False(t, c.Done())
+
+	// now all the map task completed, ask reduce task
+	argsAskTask = AskTaskArgs{WorkerId: "3"}
+	replyAskTask = AskTaskReply{}
+	err = c.AskTask(&argsAskTask, &replyAskTask)
+	fmt.Println(replyAskTask.T.toString())
+	assert.NoError(t, err)
+	assert.False(t, c.Done())
+
+	// complete the reduce task
+	argReportTask = ReportTaskStatusArgs{T: replyAskTask.T, Status: "Completed"}
+	replyReportTask = ReportTaskStatusReply{}
+	err = c.ReportTaskStatus(&argReportTask, &replyReportTask)
+	assert.NoError(t, err)
+
+	// act:
+	// assert done should return result as true
+	assert.True(t, c.Done())
 }
